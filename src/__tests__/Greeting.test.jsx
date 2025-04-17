c96501d228454014f8496bb43194aa4cd14c7568
Nazar Bapanov <bapanov_n@au.kg>
2025-04-17 10:51:06 +0600

    add testing of Hello world text

--- src/__tests__/Greeting.test.jsx
+++ src/__tests__/Greeting.test.jsx
@@ -7,3 +7,10 @@ test("Show button should be visible during first render", () =>  {
     const button = screen.queryByText("Показать")
     expect(button).toBeInTheDocument();
 });
+
+
+test("Hello world should not be visible during first render", () => {
+    render(<Greeting />);
+    const button = screen.queryByText("Привет, мир!");
+    expect(button).not.toBeInTheDocument();
+});  
